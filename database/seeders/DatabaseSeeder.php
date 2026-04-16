ea1b922a874e4681e3dc3a9f8b79af194ae948f6
Awais <50techcompany@gmail.com>
2026-04-16 06:56:24 +0500

    updated Readme.md file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        User::create([
-            'name' => 'John Doe',
-            'email' => 'superadmin@gmail.com',
-            'password' => bcrypt('password')
+        $this->call([
+            AdminSeeder::class,
+            TranslationSeeder::class,
         ]);
-        $this->call(TranslationSeeder::class);
     }
 }
